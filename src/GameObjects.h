4eb1a524909cee24c8e2fa4fab8bb7e3783e539c
samCat33 <ksammy872@gmail.com>
2025-12-06 21:42:13 -0600

    Added Documentation in GameObjects.h
    Added documentation comments for getX, getWidth, and getHeight methods in GameObjects.h for Obstacle and Collectible

--- src/GameObjects.h
+++ src/GameObjects.h
@@ -7,8 +7,20 @@ struct Obstacle {
     int type;
     bool active;
 
+    //Description: Returns x coordinate of obstacle
+    //Return: int
+    //Precondition: x coordinate of obstacle exists
+    //Postcondition: x coordinate is not modified
     int getX() const;
+    //Description: Returns width of obstacle
+    //Return: int
+    //Precondition: width of obstacle exists
+    //Postcondition: width of obstacle is not modified
     int getWidth() const;
+    //Description: Returns height of obstacle
+    //Return: int
+    //Precondition: height of obstacle exists
+    //Postcondition: height of obstacle is not modified
     int getHeight() const;
 };
 
@@ -19,6 +31,10 @@ struct Collectible {
     bool active;
     float animTime;
 
+    //Descritption: Retuns x coord of collectible
+    //Return: int
+    //Precondition: x coordinate of collectible exists
+    //Postcondition: x coordinate of collectible is not modified
     int getX() const;
 };
 
